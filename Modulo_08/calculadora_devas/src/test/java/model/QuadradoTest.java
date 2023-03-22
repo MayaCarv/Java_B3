5e0e7d1188c9024e6161dc0b0dbfac1dbc62a155
MayaCarv <mayaracarvalho@hotmail.com>
2023-03-22 20:02:11 -0300

    Atividade 1 - JUnit

--- Modulo_08/calculadora_devas/src/test/java/model/QuadradoTest.java
+++ Modulo_08/calculadora_devas/src/test/java/model/QuadradoTest.java
@@ -1,10 +1,21 @@
 package model;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class QuadradoTest {
+    Quadrado quadrado;
+
+    @Before
+    public void setup() {
+
+        quadrado = new Quadrado();
+        quadrado.setLado(5);
+    }
     @Test
     public void testarQuadrado() {
-
+        Assert.assertNotNull(quadrado);
     }
 }
+
--- Modulo_08/calculadora_devas/src/test/java/model/TrianguloTest.java
+++ Modulo_08/calculadora_devas/src/test/java/model/TrianguloTest.java
@@ -1,10 +1,21 @@
 package model;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TrianguloTest {
+    Triangulo triangulo;
+
+    @Before
+    public void setup() {
+
+        triangulo = new Triangulo();
+        triangulo.setAltura(2);
+        triangulo.setBase(5);
+    }
     @Test
     public void testarTriangulo() {
-
+        Assert.assertNotNull(triangulo);
     }
 }
